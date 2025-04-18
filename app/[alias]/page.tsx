2b2ecf2b845454e253a55fb31bce584971eb9e08
wesalshehri <113001924+wesalshehri@users.noreply.github.com>
2025-04-18 17:30:32 -0400

    Update page.tsx

--- app/[alias]/page.tsx
+++ app/[alias]/page.tsx
@@ -3,11 +3,11 @@ import clientPromise from '@/lib/mongo';
 
 export default async function Page({ params }: { params: { alias: string } }) {
   const client = await clientPromise;
-  const db = client.db(); // uses default db from URI
+  const db = client.db(); // defaults to DB name in URI
   const result = await db.collection('shorturls').findOne({ alias: params.alias });
 
   if (!result) {
-    return <div>Alias not found.</div>;
+    return <div style={{ padding: "2rem" }}>Alias not found 😢</div>;
   }
 
   redirect(result.url);
