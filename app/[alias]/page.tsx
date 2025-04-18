e48221fa8f2afe4ce4be3c2ad38d461d971485be
wesalshehri <113001924+wesalshehri@users.noreply.github.com>
2025-04-18 18:25:35 -0400

    Update page.tsx

--- app/[alias]/page.tsx
+++ app/[alias]/page.tsx
@@ -1,5 +1,5 @@
 import { redirect } from 'next/navigation';
-import clientPromise from '../../lib/mongo'; 
+import clientPromise from '../../lib/mongo';
 
 export default async function Page({
   params,
@@ -7,12 +7,12 @@ export default async function Page({
   params: { alias: string };
 }) {
   const client = await clientPromise;
-  const db = client.db();
-  const entry = await db.collection('shorturls').findOne({ alias: params.alias });
+  const db = client.db('urlshortener'); 
+  const entry = await db.collection('urls').findOne({ alias: params.alias }); 
 
   if (!entry) {
     return <div style={{ padding: '2rem' }}>Alias not found</div>;
   }
 
-  redirect(entry.url);
+  redirect(entry.url); 
 }
